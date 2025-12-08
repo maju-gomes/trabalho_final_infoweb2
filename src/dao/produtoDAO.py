321b81d89d604d69495315378cbb7ff52a97d935
maju <julia.gomes1@escolar.ifrn.edu.br>
2025-12-08 19:44:09 +0000

    tinha apagado um arquivo

--- src/dao/produtoDAO.py
+++ src/dao/produtoDAO.py
@@ -1,5 +1,5 @@
 from database import BancoDeDados
-from model.produto_produto import Produto
+from model.doacao_produto import Produto
 
 class ProdutoDAO:
     def __init__(self, banco_dados: BancoDeDados):
@@ -7,33 +7,39 @@ class ProdutoDAO:
 
     def criar_tabela(self):
         comando = """
-        CREATE TABLE IF NOT EXISTS produto(
-            ...
+        CREATE TABLE IF NOT EXISTS produto (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            descricao VARCHAR(50) NOT NULL,
+            tipo VARCHAR(50) NOT NULL,
+            quantidade INTEGER NOT NULL,
+            id_favorecido INTEGER NOT NULL,
+            FOREIGN KEY (id_favorecido) REFERENCES favorecido (id_usuario) ON DELETE CASCADE
         );
         """
         self.__banco_dados.executar(comando)
 
 
-    def listar_produto():
+    def listar_produto(self):
         comando = "SELECT id, nome, categoria FROM produto;"
         linha = self.__banco_dados.buscar(comando)
 
         lista_produtos = []
 
-        for id, nome, categoria, quantidade in linhas:
-            lista.append(Produto(
+        for id, descricao, categoria, quantidade, id_favorecido in linha:
+            lista_produtos.append(Produto(
                 id = id
-                nome = nome
+                descricao = descricao
                 categoria = categoria
                 quantidade = quantidade
+                id_favorecido = id_favorecido
             ))
 
     def listar_id_produto():
         pass
 
     def inserir_produto(self, produto: Produto):
-        comando = "INSERT INTO produto (descricao, tipo, quantidade) VALUES (?, ?, ?);"
-        parametros = (produto.self.__descricao, produto.self.__tipo, produto.self.__quantidade)
+        comando = "INSERT INTO produto (descricao, categoria, quantidade) VALUES (?, ?, ?);"
+        parametros = (produto.descricao, produto.tipo, produto.quantidade)
         self.__banco_dados.executar(comando, parametros)
 
     def atualizar_produto():
