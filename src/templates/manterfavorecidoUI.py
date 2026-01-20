69d5a2c1450cdf91607af2aba0bf8864ba9605b2
maju <julia.gomes1@escolar.ifrn.edu.br>
2026-01-20 14:31:02 +0000

    correcoes codigo 2

--- src/templates/manterfavorecidoUI.py
+++ src/templates/manterfavorecidoUI.py
@@ -13,18 +13,18 @@ class ManterFavorecidoUI:
     def inserir():
         with st.form('ins_fav'):
             with st.expander('Dados Pessoais', expanded=True):
-                nome = st.text_input('Informe o nome', key='ins_fav_nome')
-                email = st.text_input('Informe o e-mail', key='ins_fav_email')
-                senha = st.text_input('Informe a senha', key='ins_fav_senha', type='password')
-                cpf = st.text_input('Informe o CPF', key='ins_fav_cpf')
+                nome = st.text_input('Informe o nome *', key='ins_fav_nome')
+                email = st.text_input('Informe o e-mail *', key='ins_fav_email')
+                senha = st.text_input('Informe a senha *', key='ins_fav_senha', type='password')
+                cpf = st.text_input('Informe o CPF *', key='ins_fav_cpf')
                 fone = st.text_input('Informe o telefone', key='ins_fav_fone')
             with st.expander('Dados de Endereço'):
-                cep = st.text_input('Informe o CEP', key='ins_fav_cep')
-                uf = st.text_input('Informe a Unidade Federativa (UF)', key='ins_fav_uf')
-                ci = st.text_input('Informe a cidade', key='ins_fav_cidade')
-                ba = st.text_input('Informe o bairro', key='ins_fav_bairro')
-                rua = st.text_input('Informe a rua', key='ins_fav_rua')
-                num = st.text_input('Informe o número', key='ins_fav_num')
+                cep = st.text_input('Informe o CEP *', key='ins_fav_cep')
+                uf = st.text_input('Informe a Unidade Federativa (UF) *', key='ins_fav_uf')
+                ci = st.text_input('Informe a cidade *', key='ins_fav_cidade')
+                ba = st.text_input('Informe o bairro *', key='ins_fav_bairro')
+                rua = st.text_input('Informe a rua *', key='ins_fav_rua')
+                num = st.text_input('Informe o número *', key='ins_fav_num')
                 com = st.text_input('Informe o complemento', key='ins_fav_com')
             submit = st.form_submit_button('Cadastrar')
             if submit:
@@ -60,19 +60,19 @@ class ManterFavorecidoUI:
                 op = st.session_state.fav
                 end = EnderecoView.listar_id(op.get_id_endereco())
                 with st.expander('Dados Pessoais', expanded=True):
-                    nome = st.text_input('Informe o nome', op.get_nome(), key='upd_fav_nome')
-                    email = st.text_input('Informe o e-mail', op.get_email(), key='upd_fav_email')
-                    senha = st.text_input('Informe a senha', op.get_senha(), key='upd_fav_senha', type='password')
+                    nome = st.text_input('Informe o nome *', op.get_nome(), key='upd_fav_nome')
+                    email = st.text_input('Informe o e-mail *', op.get_email(), key='upd_fav_email')
+                    senha = st.text_input('Informe a senha *', op.get_senha(), key='upd_fav_senha', type='password')
                     cpf_old = f"{op.get_cpf()[:3]}.{op.get_cpf()[3:6]}.{op.get_cpf()[6:9]}-{op.get_cpf()[9:]}"
-                    cpf = st.text_input('Informe o CPF', cpf_old, key='upd_fav_cpf')
+                    cpf = st.text_input('Informe o CPF *', cpf_old, key='upd_fav_cpf')
                     fone = st.text_input('Informe o telefone', op.get_telefone(), key='upd_fav_fone')
                 with st.expander('Dados de Endereço'):
-                    cep = st.text_input('Informe o CEP', end.get_cep(), key='upd_fav_cep')
-                    uf = st.text_input('Informe a Unidade Federativa (UF)', end.get_uf(), key='upd_fav_uf')
-                    ci = st.text_input('Informe a cidade', end.get_cidade(), key='upd_fav_cidade')
-                    ba = st.text_input('Informe o bairro', end.get_bairro(), key='upd_fav_bairro')
-                    rua = st.text_input('Informe a rua', end.get_rua(), key='upd_fav_rua')
-                    num = st.text_input('Informe o número', end.get_numero(), key='upd_fav_num')
+                    cep = st.text_input('Informe o CEP *', end.get_cep(), key='upd_fav_cep')
+                    uf = st.text_input('Informe a Unidade Federativa (UF) *', end.get_uf(), key='upd_fav_uf')
+                    ci = st.text_input('Informe a cidade *', end.get_cidade(), key='upd_fav_cidade')
+                    ba = st.text_input('Informe o bairro *', end.get_bairro(), key='upd_fav_bairro')
+                    rua = st.text_input('Informe a rua *', end.get_rua(), key='upd_fav_rua')
+                    num = st.text_input('Informe o número *', end.get_numero(), key='upd_fav_num')
                     com = st.text_input('Informe o complemento', end.get_complemento(), key='upd_fav_com')
                 submit = st.form_submit_button('Atualizar')
                 if submit:
